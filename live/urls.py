51bff0cc1ca4b6b4263ef89f94dac6955761ad88
DESKTOP-VTA8OCP\JUAN JOSE <juan.merlos@uab.edu.bo>
2024-09-17 15:05:40 -0400

    autenticacion con JWT integrado en la ruta principal

--- live/urls.py
+++ live/urls.py
@@ -3,11 +3,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+
+    # Autenticación JWT
+    path('', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
