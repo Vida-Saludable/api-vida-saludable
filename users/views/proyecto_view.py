b83c3b956d4d49be66d29ac60988b2fe5a9dd885
DESKTOP-VTA8OCP\JUAN JOSE <juan.merlos@uab.edu.bo>
2024-10-09 18:36:47 -0400

    lista de proyectos de cada usuario

--- users/views/proyecto_view.py
+++ users/views/proyecto_view.py
@@ -54,48 +54,48 @@ class ProyectoViewSet(viewsets.ModelViewSet):
     def perform_destroy(self, instance):
         instance.delete()
 
-class ListaProyectoUsuariosView(APIView):
-    def get(self, request, usuario_id):
-        try:
-            # Verificar que el usuario exista
-            usuario = Usuario.objects.get(id=usuario_id)
+# class ListaProyectoUsuariosView(APIView):
+#     def get(self, request, usuario_id):
+#         try:
+#             # Verificar que el usuario exista
+#             usuario = Usuario.objects.get(id=usuario_id)
 
-            # Obtener los proyectos asociados al usuario mediante la relación UsuarioProyecto
-            usuario_proyectos = UsuarioProyecto.objects.filter(usuario=usuario)
+#             # Obtener los proyectos asociados al usuario mediante la relación UsuarioProyecto
+#             usuario_proyectos = UsuarioProyecto.objects.filter(usuario=usuario)
 
-            if not usuario_proyectos.exists():
-                return Response(
-                    {"message": "Este usuario no tiene proyectos asociados."},
-                    status=status.HTTP_404_NOT_FOUND
-                )
+#             if not usuario_proyectos.exists():
+#                 return Response(
+#                     {"message": "Este usuario no tiene proyectos asociados."},
+#                     status=status.HTTP_404_NOT_FOUND
+#                 )
             
-            # Extraer los proyectos relacionados con el usuario
-            proyectos = [usuario_proyecto.proyecto for usuario_proyecto in usuario_proyectos]
+#             # Extraer los proyectos relacionados con el usuario
+#             proyectos = [usuario_proyecto.proyecto for usuario_proyecto in usuario_proyectos]
 
-            # Serializar los proyectos
-            serializer = ProyectoSerializer(proyectos, many=True)
+#             # Serializar los proyectos
+#             serializer = ProyectoSerializer(proyectos, many=True)
             
-            # Filtrar solo los campos deseados (id, nombre y descripcion) y agrupar en 'data'
-            proyectos_filtrados = [
-                {
-                    "id": proyecto["id"],  # Incluir el ID del proyecto
-                    "nombre": proyecto["nombre"],
-                    "descripcion": proyecto["descripcion"]
-                } for proyecto in serializer.data
-            ]
+#             # Filtrar solo los campos deseados (id, nombre y descripcion) y agrupar en 'data'
+#             proyectos_filtrados = [
+#                 {
+#                     "id": proyecto["id"],  # Incluir el ID del proyecto
+#                     "nombre": proyecto["nombre"],
+#                     "descripcion": proyecto["descripcion"]
+#                 } for proyecto in serializer.data
+#             ]
             
-            # Crear la respuesta final con 'success', 'message' y 'data' (proyectos)
-            response_data = {
-                "success": True,
-                "message": "Proyectos obtenidos exitosamente.",
-                "data": proyectos_filtrados  # Proyectos dentro de 'data'
-            }
+#             # Crear la respuesta final con 'success', 'message' y 'data' (proyectos)
+#             response_data = {
+#                 "success": True,
+#                 "message": "Proyectos obtenidos exitosamente.",
+#                 "data": proyectos_filtrados  # Proyectos dentro de 'data'
+#             }
             
-            # Devolver la respuesta en formato JSON
-            return Response(response_data, status=status.HTTP_200_OK)
+#             # Devolver la respuesta en formato JSON
+#             return Response(response_data, status=status.HTTP_200_OK)
 
-        except Usuario.DoesNotExist:
-            return Response(
-                {"error": "Usuario no encontrado"},
-                status=status.HTTP_404_NOT_FOUND
-            )
+#         except Usuario.DoesNotExist:
+#             return Response(
+#                 {"error": "Usuario no encontrado"},
+#                 status=status.HTTP_404_NOT_FOUND
+#             )
--- users/views/usuario_proyecto_view.py
+++ users/views/usuario_proyecto_view.py
@@ -1,8 +1,61 @@
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
+from users.models.usuario_models import Usuario
 from users.models.usuario_proyecto_model import UsuarioProyecto
+from users.serializers.proyecto_serializer import ProyectoSerializer
 from users.serializers.usuario_proyecto_serializer import UsuarioProyectoSerializer
+
 class UsuarioProyectoViewSet(viewsets.ModelViewSet):
     queryset = UsuarioProyecto.objects.all()
     serializer_class = UsuarioProyectoSerializer
     # permission_classes = [IsAuthenticated]
+
+class ListaProyectosPorUsuarioView(APIView):
+    
+    def get(self, request, usuario_id):
+        try:
+            # Verificar que el usuario exista
+            usuario = Usuario.objects.get(id=usuario_id)
+
+            # Obtener los proyectos asociados al usuario mediante la relación UsuarioProyecto
+            usuario_proyectos = UsuarioProyecto.objects.filter(usuario=usuario)
+
+            if not usuario_proyectos.exists():
+                return Response(
+                    {"message": "Este usuario no tiene proyectos asociados."},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+            
+            # Extraer los proyectos relacionados con el usuario
+            proyectos = [usuario_proyecto.proyecto for usuario_proyecto in usuario_proyectos]
+
+            # Serializar los proyectos
+            serializer = ProyectoSerializer(proyectos, many=True)
+            
+            # Filtrar solo los campos deseados (id, nombre y descripcion) y agrupar en 'data'
+            proyectos_filtrados = [
+                {
+                    "id": proyecto["id"],  # Incluir el ID del proyecto
+                    "nombre": proyecto["nombre"],
+                    "descripcion": proyecto["descripcion"]
+                } for proyecto in serializer.data
+            ]
+            
+            # Crear la respuesta final con 'success', 'message' y 'data' (proyectos)
+            response_data = {
+                "success": True,
+                "message": "Proyectos obtenidos exitosamente.",
+                "data": proyectos_filtrados  # Proyectos dentro de 'data'
+            }
+            
+            # Devolver la respuesta en formato JSON
+            return Response(response_data, status=status.HTTP_200_OK)
+
+        except Usuario.DoesNotExist:
+            return Response(
+                {"error": "Usuario no encontrado"},
+                status=status.HTTP_404_NOT_FOUND
+            )
